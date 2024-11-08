ca7df3ee9fcb55f1a0df8ab34884f88929d1a956
Goran Belanovic <goranbelanovic@yahoo.com>
2024-11-08 20:07:56 +0100

    msg

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ const OpenAI= require('openai');
 const API_KEY = process.env.chatGPT_key;
 
 app.use(function (req, res, next) {  
-  
     res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
-
-
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    
-  
     res.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     next()
 });
@@ -22,25 +17,25 @@ app.use(express.json({
 }));
 
 app.post('/chatGPT', async (req, res) => {
+  
     const text = req.body.text;
-    console.log(text);
-    /* const text = 'ovo je neki tekst kita dupe dalje nse nastavlja picka'; */
-    let answer = await callChatGPT(text);
+    const prompt = req.body.prompt;
+    const description = req.body.description;
+    
+    console.log('evo me')
+    console.log(prompt)
+    console.log(description)
+
+    let answer = await callChatGPT(text, prompt, description);
     
     console.log(answer);
     console.log(typeof answer);
     res.json(answer);
 })
 
+async function callChatGPT(text, prompt, description) {
 
-async function callChatGPT(text) {
-
- /* const prompt = `take this text in serbian and check if there is any swearing words in it. If you found them, put them in the array [] and response me only thay array. If you don't find them, return only empty array [] ` + text; */
- /* const prompt = `узмите овај текст на српском и проверите да ли има речи и реченица са правописним или граматичким грешкама. Ако сте их пронашли, ставите те речи и реченице у низ [] и одговорите ми само у том низу. Ако не пронађете речи или реченице са правописним или граматичким грешкама, вратите само празан низ [] ` + text;; */
- const prompt = `детаљно прегледај текст на српском, пронађи граматичке грешке. Стави те речи у низ [] и одговори ми само у том низу. Ако нема грешака, врати празан низ []. Ево текста: ` + text;
-
-
-    try {
+  try {
       const response = await fetch('https://api.openai.com/v1/chat/completions', {
         method: 'POST',
         headers: {
@@ -48,15 +43,15 @@ async function callChatGPT(text) {
           'Authorization': `Bearer ${API_KEY}`
         },
         body: JSON.stringify({
-          model: 'gpt-4', // Using 'gpt-4-turbo' as a placeholder
+          model: 'gpt-4o',
           messages: [
-            { role: 'system', content: 'Ви сте асистент за проверу граматике са знањем српског језика. Пажљиво анализирајте дати текст да ли постоје граматичке, правописне или стилске грешке' },
-            { role: 'user', content: prompt}
+            { role: 'system', content: description},
+            { role: 'user', content: prompt + text}
           ],
           temperature: 0.2,
           max_tokens: 150,
           top_p: 1
-           // Customize this value based on your needs
+
         })
       });
   
