5832f3082c5a087e6db722c616b6fd3895acf1de
belanovic <goranbelanovic@yahoo.com>
2024-11-10 03:03:15 +0100

    msg

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const OpenAI= require('openai');
 const API_KEY = process.env.chatGPT_key;
 
 app.use(function (req, res, next) {  
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
+    if(req.headers.origin) res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     next()
